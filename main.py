29161b2fe9b98048eac8cbccee4f142b2ea282d8
wuyuqq <33753437+wuyuqq@users.noreply.github.com>
2024-04-07 17:19:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -103,12 +103,18 @@ def parse_list_file(link, output_directory):
 
     for pattern, addresses in df.groupby('pattern')['address'].apply(list).to_dict().items():
         if pattern == 'domain_suffix':
-            rule_entry = {pattern: [address.strip() for address in addresses]}
+            rule_entry = {pattern: ['.' + address.strip() for address in addresses]}
             result_rules["rules"].append(rule_entry)
+            domain_entries.extend([address.strip() for address in addresses])
+        elif pattern == 'domain':
             domain_entries.extend([address.strip() for address in addresses])
         else:
             rule_entry = {pattern: [address.strip() for address in addresses]}
             result_rules["rules"].append(rule_entry)
+    # 删除 'domain_entries' 中的重复值
+    domain_entries = list(set(domain_entries))
+    if domain_entries:
+        result_rules["rules"].insert(0, {'domain': domain_entries})
 
     # 使用 output_directory 拼接完整路径
     file_name = os.path.join(output_directory, f"{os.path.basename(link).split('.')[0]}.json")
