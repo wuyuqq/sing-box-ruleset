b6295f53ecb347cb3e0e7787f1410afd76cb9633
wuyuqq <33753437+wuyuqq@users.noreply.github.com>
2024-04-07 17:03:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -111,9 +111,6 @@ def parse_list_file(link, output_directory):
         else:
             rule_entry = {pattern: [address.strip() for address in addresses]}
             result_rules["rules"].append(rule_entry)
-    # 删除 'domain_entries' 中的重复值
-    if domain_entries:
-        result_rules["rules"].insert(0, {'domain': domain_entries})
 
     # 使用 output_directory 拼接完整路径
     file_name = os.path.join(output_directory, f"{os.path.basename(link).split('.')[0]}.json")
